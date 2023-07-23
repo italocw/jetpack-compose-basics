4a086076d55ec1fb17507a9e2adf064c602ce96b
Ítalo Bezerra <italobezerra.cw@gmail.com>
2023-07-23 05:55:46 -0300

    Finishing touches!

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -4,36 +4,38 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.spring
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
-
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.text.font.FontWeight
 
 @Composable
 fun OnboardingScreen(modifier: Modifier = Modifier, onContinueClicked: () -> Unit) {
@@ -57,9 +59,10 @@ fun OnboardingScreen(modifier: Modifier = Modifier, onContinueClicked: () -> Uni
 @Composable
 fun OnboardingPreview() {
     BasicsCodelabTheme {
-        OnboardingScreen (onContinueClicked = {})
+        OnboardingScreen(onContinueClicked = {})
     }
 }
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -77,12 +80,13 @@ class MainActivity : ComponentActivity() {
 
         Surface(modifier) {
             if (shouldShowOnboarding) {
-                OnboardingScreen( onContinueClicked = {shouldShowOnboarding=false})
+                OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
             } else {
                 Greetings()
             }
         }
     }
+
     @Preview
     @Composable
     fun MyAppPreview() {
@@ -114,40 +118,65 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     private fun Greeting(name: String) {
-
-        var expanded by remember { mutableStateOf(false) }
-
-        val extraPadding by animateDpAsState(
-            if (expanded) 48.dp else 0.dp,
-            animationSpec = spring(
-                dampingRatio = Spring.DampingRatioMediumBouncy,
-                stiffness = Spring.StiffnessLow
-            )
-        )
-
-        Surface(
-            color = MaterialTheme.colorScheme.primary,
+        Card(
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.primary
+            ),
             modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
         ) {
-            Row(modifier = Modifier.padding(24.dp)) {
-                Column(modifier = Modifier
+            CardContent(name)
+        }
+    }
+
+    @Composable
+    private fun CardContent(name: String) {
+        var expanded by remember { mutableStateOf(false) }
+
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+                .animateContentSize()
+        ) {
+            Column(
+                modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-                ) {
-                    Text(text = "Hello, ")
+
+            ) {
+                Text(text = "Hello, ")
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
+                if (expanded) {
                     Text(
-                        text = name,
-                        style = MaterialTheme.typography.headlineMedium.copy(
-                            fontWeight = FontWeight.ExtraBold
-                        )
-                    )                }
-                ElevatedButton(
-                    onClick = { expanded = !expanded }
-                ) {
-                    Text(if (expanded) "Show less" else "Show more")
+                        text = ("Composem ipsum color sit lazy, " +
+                                "padding theme elit, sed do bouncy. ").repeat(4),
+                    )
+                }
+            }
+
+
+            IconButton(
+                onClick = { expanded = !expanded }
+            ) {
+                val imageVector:ImageVector
+                val contentDescription: String
+
+                if (expanded) {
+                        imageVector =   Icons.Filled.ExpandLess
+                        contentDescription = getString(R.string.show_less)
+
+                } else {
+                        imageVector =   Icons.Filled.ExpandMore
+                        contentDescription = getString(R.string.show_more)
                 }
 
+                Icon(imageVector = imageVector, contentDescription =contentDescription )
             }
+
+
         }
     }
 
